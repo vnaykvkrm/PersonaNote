c0c0f8a52a007b0d6c515a318603e437c75ff68f
vinayak vikram <vnaykvkrm@gmail.com>
2023-07-07 23:15:54 +0530

    input in form

--- personanote/src/components/customForm.jsx
+++ personanote/src/components/customForm.jsx
@@ -11,6 +11,16 @@ function CustomForm() {
     onSubmit={handleFormSubmit}
     >
         <div className="wrapper">
+            <input type="text" 
+            id='task'
+            className='input'
+            value={task}
+            onInput={(e) => setTask(e.target.value)}
+            required
+            autoFocus
+            maxLength={60}
+            placeholder='Enter a Persona Note(task)'
+            />
 
         </div>
 
