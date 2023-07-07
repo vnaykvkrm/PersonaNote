bf58dcc48525c9adf66b3cb2cceb1d4d7e48b41d
vinayak vikram <vnaykvkrm@gmail.com>
2023-07-07 23:11:43 +0530

    edited app.jsx

--- personanote/src/App.jsx
+++ personanote/src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import CustomForm from './components/customForm'
 import './App.css'
 
 function App() {
   return (
-    <div className="container">
+    <div className="App">
+      <CustomForm />
       <header>
         <h1> Personal Tasks & Notes</h1>
       </header>
