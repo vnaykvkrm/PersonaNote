7c138185940e8583dac9f8067b3309946f9c3f82
vinayak vikram <vnaykvkrm@gmail.com>
2023-07-16 22:17:21 +0530

    created a file named Personalist

--- personanote/src/App.jsx
+++ personanote/src/App.jsx
@@ -2,11 +2,14 @@ import React,{useState} from 'react'
 import CustomForm from './components/customForm'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [personas, setPersonas] = useState([]);
 
   const addPersona = (persona) => {
-    console.log(persona)
-  }
+    setPersonas(prevState=>[...prevState,persona])   // line uses the setPersonas function to update the personas state.
+                                                  // It takes the previous state (prevState) and appends the new persona to it. 
+                                                  //The syntax [...prevState, persona] creates a new array by spreading the elements
+                                                  // of the previous state array and adding the new persona at the end.
+  } 
   return (
     <div className="App">
       <header>
@@ -14,8 +17,9 @@ function App() {
       </header>
 
       <CustomForm addPersona= {addPersona}/>
+      {personas && <PersonaList/>}
     </div>
   )
-}
+} 
 
 export default App
